ba39536ae546a22cac8262c04b30191505c6458b
Matt Cherry <m495c301@ku.edu>
2018-11-05 12:09:06 -0600

    Testing

--- main.cpp
+++ main.cpp
@@ -13,7 +13,20 @@ int main(int argc, char** argv)
 {
 	Test foo;
 	foo.runTest();
+	LinkedListOfInts a;
+	for (int i = 0; i < 10; i++)
+	{
+		a.addFront(i);
+	}
+	std::vector<int> temp = a.toVector();
+	std::cout << "Real Value: ";
+	for(int i = 0; i < temp.size(); i++)
+	{
+		std::cout << temp.at(i) << ' ';
+	}
+	std::cout << std::endl;
 
+	std::cout << "Expected: 0 1 2 3 4 5 6 7 8 9\n";
 	std::cout << "Running...\nAnd we're done.\nGoodbye.\n";
 
 	return (0);
